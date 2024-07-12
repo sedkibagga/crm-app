e8315378a6ff175c0306cdc0dc0c50f254a354da
MedRiadh1 <msridi13@gmail.com>
2024-07-12 15:16:33 +0100

    changes on Entity PointDeVente

--- users-microservice/src/point-de-vente/dtos/updatePointDeVente.dto.ts
+++ users-microservice/src/point-de-vente/dtos/updatePointDeVente.dto.ts
@@ -1,31 +1,3 @@
-// import { IsString, IsNumber, IsOptional } from "class-validator";
-
-// export class UpdatePointDeVenteDto{
-//     @IsString()
-//     @IsOptional()
-//     nom: string
-
-//     @IsString()
-//     @IsOptional()
-//     prenom: string
-
-//     @IsString()
-//     @IsOptional()
-//     secteur_activite: string
-
-//     @IsNumber()
-//     @IsOptional()
-//     num_tel: number
-
-//     @IsString()
-//     @IsOptional()
-//     localisation: string
-
-//     @IsString()
-//     @IsOptional()
-//     decision: string
-// }
-
 import { PartialType } from '@nestjs/mapped-types';
 import { PointDeVenteDto } from './pointDeVente.dto';
 
